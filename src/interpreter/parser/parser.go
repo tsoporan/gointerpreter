2d72840441c707ae569bbc3915699fe2bc3fdf8c
Titus Soporan <titus@tsoporan.com>
2018-07-17 12:52:28 -0400

    Add parseReturnStatement to parser
    Currently skips everything until SEMICOLON
    
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- src/interpreter/parser/parser.go
+++ src/interpreter/parser/parser.go
@@ -73,6 +73,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+  case token.RETURN:
+    return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -99,6 +101,21 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	return stmt
+}
+
+/*
+  Parse a RETURN statement
+*/
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+  stmt := &ast.ReturnStatement{Token: p.curToken}
+
+  p.nextToken()
+
+  for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
+
+  return stmt
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
